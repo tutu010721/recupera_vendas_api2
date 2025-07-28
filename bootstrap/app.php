0397b0b20a2112a87fc1afc59c7df6a0b56c18cc
tutu010721 <sac.auramoveiseutilidades@gmail.com>
2025-07-27 22:04:47 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php', // <-- A LINHA QUE FALTAVA
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
